73d39d2185cd4ac357883cc14289881a74ef34fa
Dennis Richter <richter.dennis1985@gmail.com>
2025-08-19 12:08:41 +0200

    Added blockquote and footer elements

--- got-quote-generator/src/App.js
+++ got-quote-generator/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 
+const quote = "Der Winter naht (Winter is Coming)";
+const character = "Ned Stark";
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +11,14 @@ function App() {
              <h1>Game of Thrones Zitate Generator</h1>
              <p>Ein Ort für Weisheit (und Sarkasmus) aus Westeros.</p>
       </header>
+      <main>
+        <blockquote>
+          "{quote}"
+        </blockquote>
+      </main>
+      <footer>
+        - {character}
+      </footer>
     </div>
   );
 }
